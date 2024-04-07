3f59ac49824d53e4ebea9c43f0074a67e2736731
Quinn Qiu <quinnqiu0823@icloud.com>
2024-04-07 16:36:37 +0800

    New - v7.0!

---
+++ v7/v7.0/js/command.js
@@ -0,0 +1,12 @@
+function command() {
+    var command = document.getElementById('command').value;
+    if (command == "Home" || command == "home") {
+        window.location = "";
+    } else if (command == "About" || command == "about") {
+        window.location = "about.html";
+    } else if (command == "Test" || command == "test") {
+        window.location = "test.html";
+    } else {
+        alert("Error");
+    }
+}
